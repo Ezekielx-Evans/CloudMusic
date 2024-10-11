46b99da52ac5f187b3d541130bcbb4d3847c9a89
Ezekielx <ezekielx.evans@gmail.com>
2024-10-11 15:16:19 +0800

    侧边栏

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <template>
-
+    <Sidebar />
 </template>
 
 <script setup>
-
+    import Sidebar from './components/Sidebar.vue';
 </script>
-
 <style scoped>
 
 </style>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,5 +1,21 @@
 <template>
-
+    <div class="sidebar">
+        <div class="menu">
+            首页
+        </div>
+        <div class="menu">
+            排行榜
+        </div>
+        <div class="menu">
+            歌单
+        </div>
+        <div class="menu">
+            MV
+        </div>
+        <div class="menu">
+            歌手
+        </div>
+    </div>
 </template>
 
 <script setup>
@@ -7,5 +23,12 @@
 </script>
 
 <style scoped>
+.sidebar {
+    display: flex;
+    flex-direction: column;
+}
 
+.menu {
+    width: 50px;
+}
 </style>
